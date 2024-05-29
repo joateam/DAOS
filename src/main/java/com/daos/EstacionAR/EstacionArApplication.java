54992abd1d4bb5d1b8997ba08af3ecb200b6b31c
solangeMaricel <sanderson@frsf.utn.edu.ar>
2024-05-29 09:27:58 -0300

    inicio de proyecto

---
+++ src/main/java/com/daos/EstacionAR/EstacionArApplication.java
@@ -0,0 +1,13 @@
+package com.daos.EstacionAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionArApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionArApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daos/EstacionAR/EstacionArApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daos.EstacionAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
