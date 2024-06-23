bff857fdf75d28b22f0bf03aba8c096c3be93f4f
Joaquín D. Giuliano <jgiuliano@frsf.utn.edu.ar>
2024-06-22 22:11:09 -0300

    Add AbonoComercio

--- src/main/java/com/daos/EstacionAR/Controller/AbonoComercioController.java
+++ src/main/java/com/daos/EstacionAR/Controller/AbonoComercioController.java
@@ -7,17 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class AbonoComercioController {
 
-	@GetMapping("/abonoComercio")
+	@GetMapping(value = "", proceces)
     public ResponseEntity<String> getAbonoComercio(@RequestParam(value = "id", required = false) String id) {
         // Lógica para manejar la solicitud GET
         return ResponseEntity.ok("Solicitud GET recibida con id: " + id);
-    }
-
-    // Método POST
-    @PostMapping("/abonoComercio")
-    public ResponseEntity<String> postAbonoComercio(@RequestBody AbonoComercioRequest request) {
-        // Lógica para manejar la solicitud POST
-        return ResponseEntity.ok("Solicitud POST recibida con datos: " + request);
     }
 	
 }
