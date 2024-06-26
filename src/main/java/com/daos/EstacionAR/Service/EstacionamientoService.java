0dc8cfebcfd007f1955b6e0ab88f92ecf29e1900
NaySauer <100716950+NaySauer@users.noreply.github.com>
2024-06-26 14:34:26 -0300

    Add files via upload
    implementacion EstacionamientoService

---
+++ src/main/java/com/daos/EstacionAR/Service/EstacionamientoService.java
@@ -0,0 +1,44 @@
+package com.daos.EstacionAR.Service;
+
+import com.daos.EstacionAR.Controller.EstacionamientoForm;
+import com.daos.EstacionAR.Entity.Estacionamiento;
+import com.daos.EstacionAR.Repository.IEstacionamientoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EstacionamientoService {
+
+     @Autowired
+    private IEstacionamientoRepository estacionamientoRepository;
+
+    // Interactua con el repositorio para validar la contraseña del usuario y asegurarse de que no haya operaciones duplicadas
+    public void validarEstacionamiento(EstacionamientoForm estacionamientoForm) throws Exception {
+        // validacion
+    }
+
+    // Inicia un nuevo registro de estacionamiento
+    public Estacionamiento crearEstacionamiento(EstacionamientoForm estacionamientoForm) {
+        Estacionamiento nuevoEstacionamiento = new Estacionamiento();
+        nuevoEstacionamiento.setPatente(estacionamientoForm.getPatente());
+        nuevoEstacionamiento.setPasswordUser(estacionamientoForm.getPasswordUser());
+        nuevoEstacionamiento.setEstado(estacionamientoForm.getEstado() == EstacionamientoForm.EstadoEstacionamiento.ESTACIONADO);
+
+        return estacionamientoRepository.save(nuevoEstacionamiento);
+    }
+
+    // Interactura con el repositorio para actualizar un registro de estacionamiento existente
+    public Optional<Estacionamiento> actualizarEstacionamiento(String patente, EstacionamientoForm estacionamientoForm) {
+        // lógica
+        return Optional.empty();
+    }
+
+    // Cnsultar un registro de estacionamiento por patente
+    public Optional<Estacionamiento> consultarEstacionamiento(String patente) {
+        // lógica
+        return Optional.empty();
+    }
+}
