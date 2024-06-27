4bfc5798aee8517276ea6997896f28ce451a6d20
Matias Calvento <calventomatias@gmail.com>
2024-06-27 17:07:47 -0300

    corregido un error falta de ID en la entity

--- src/main/java/com/daos/EstacionAR/Entity/Recarga.java
+++ src/main/java/com/daos/EstacionAR/Entity/Recarga.java
@@ -6,12 +6,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 
 
 @Entity
 public class Recarga {
-
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
