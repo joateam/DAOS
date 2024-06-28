7f1c6c5f729ec7c7226e1467eee2860d0d7613f4
NaySauer <100716950+NaySauer@users.noreply.github.com>
2024-06-28 12:18:02 -0300

    controller general

--- src/main/java/com/daos/EstacionAR/Controller/EstacionamientoController.java
+++ src/main/java/com/daos/EstacionAR/Controller/EstacionamientoController.java
@@ -10,48 +10,54 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/estacionamientos") //URL base
+@RequestMapping(value = "/estacionamientos") // URL base
 public class EstacionamientoController {
 
     @Autowired
     private EstacionamientoService estacionamientoService;
 
-    //CREAR ESTACIONAMIENTO, nuevo registro utilizando el form
+    // CREAR ESTACIONAMIENTO, nuevo registro utilizando el form
+
     @PostMapping
+    @ResponseBody
     public ResponseEntity<?> crearEstacionamiento(@Validated @RequestBody EstacionamientoForm estacionamientoForm) {
         try {
-            estacionamientoService.validarEstacionamiento(estacionamientoForm);
-            Estacionamiento nuevoEstacionamiento = estacionamientoService.crearEstacionamiento(estacionamientoForm);
-            return ResponseEntity.ok(nuevoEstacionamiento); //exitoso
+            Estacionamiento nuevoEstacionamiento = estacionamientoService.validarYCrearEstacionamiento(estacionamientoForm);
+            return ResponseEntity.ok(nuevoEstacionamiento); // exitoso
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage()); //msj de error
+            return ResponseEntity.badRequest().body(e.getMessage()); // msj de error
         }
     }
 
     // ACTUALIZAR ESTACIONAMIENTO
-    @PutMapping("/{patente}")
-    public ResponseEntity<?> actualizarEstacionamiento(@PathVariable String patente, @Validated @RequestBody EstacionamientoForm estacionamientoForm) {
+
+    @PutMapping(value = "/{patente}") // es lo mismo q hacer esto...
+    // @RequestMapping (value = "/{patente}"), method = RequestMethod.PutMapping)
+
+    public ResponseEntity<?> actualizarEstacionamiento(@PathVariable String patente,
+            @Validated @RequestBody EstacionamientoForm estacionamientoForm) {
         try {
             estacionamientoService.validarEstacionamiento(estacionamientoForm);
-            Optional<Estacionamiento> estacionamientoActual = estacionamientoService.actualizarEstacionamiento(patente, estacionamientoForm);
+            Optional<Estacionamiento> estacionamientoActual = estacionamientoService.actualizarEstacionamiento(patente,
+                    estacionamientoForm);
             if (estacionamientoActual.isPresent()) {
                 return ResponseEntity.ok(estacionamientoActual.get());
             } else {
-                return ResponseEntity.notFound().build(); //no encuentro el estacionamiento
+                return ResponseEntity.notFound().build(); // no encuentro el estacionamiento
             }
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
-    //CONSULTAR ESTADO DEL ESTACIONAMIENTO, por patente
-    @GetMapping("/{patente}")
+    // CONSULTAR ESTADO DEL ESTACIONAMIENTO, por patente
+    @GetMapping(value = "/{patente}")
     public ResponseEntity<?> consultarEstacionamiento(@PathVariable String patente) {
         Optional<Estacionamiento> estacionamiento = estacionamientoService.consultarEstacionamiento(patente);
         if (estacionamiento.isPresent()) {
-            return ResponseEntity.ok(estacionamiento.get()); //devuelvo detalles
+            return ResponseEntity.ok(estacionamiento.get()); // devuelvo detalles
         } else {
-            return ResponseEntity.notFound().build(); //no ahy estacionamiento
+            return ResponseEntity.notFound().build(); // no ahy estacionamiento
         }
     }
 }
