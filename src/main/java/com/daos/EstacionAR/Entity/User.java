70da6017003fcb6d6bfb8116341a443fa8d366f5
Joaquín D. Giuliano <jgiuliano@frsf.utn.edu.ar>
2024-07-01 20:36:44 -0300

    Agregamos getters and setters

--- src/main/java/com/daos/EstacionAR/Entity/User.java
+++ src/main/java/com/daos/EstacionAR/Entity/User.java
@@ -12,10 +12,6 @@ import lombok.Setter;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 
 public class User {
     @Id
@@ -28,4 +24,69 @@ public class User {
     private LocalDate nacimiento;
     @OneToOne(cascade = CascadeType.ALL)
     private Vehiculo vehiculo;
+	public Integer getDni() {
+		return dni;
+	}
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public LocalDate getNacimiento() {
+		return nacimiento;
+	}
+	public void setNacimiento(LocalDate nacimiento) {
+		this.nacimiento = nacimiento;
+	}
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	public User(Integer dni, String nombre, String apellido, String contraseña, String domicilio, String correo,
+			LocalDate nacimiento, Vehiculo vehiculo) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.contraseña = contraseña;
+		this.domicilio = domicilio;
+		this.correo = correo;
+		this.nacimiento = nacimiento;
+		this.vehiculo = vehiculo;
+	}
+    
+	public User() {
+		super();
+	}
+    
 }
--- src/main/java/com/daos/EstacionAR/Entity/Vehiculo.java
+++ src/main/java/com/daos/EstacionAR/Entity/Vehiculo.java
@@ -8,13 +8,40 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Vehiculo {
     @Id
     private String patente;
     private String marca;
     private String modelo;
+	public String getPatente() {
+		return patente;
+	}
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public Vehiculo(String patente, String marca, String modelo) {
+		super();
+		this.patente = patente;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	public Vehiculo() {
+		super();
+	}
+	
+
+ 
 }
