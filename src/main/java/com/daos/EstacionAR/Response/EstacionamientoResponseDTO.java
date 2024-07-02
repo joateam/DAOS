800957112378c9c575d0bbb2b5abeb92c811e755
NaySauer <100716950+NaySauer@users.noreply.github.com>
2024-07-02 10:07:54 -0300

    delete user

--- src/main/java/com/daos/EstacionAR/Response/EstacionamientoResponseDTO.java
+++ src/main/java/com/daos/EstacionAR/Response/EstacionamientoResponseDTO.java
@@ -6,18 +6,14 @@ public class EstacionamientoResponseDTO {
 
     private Long id;
     private String patente;
-    private String contraseña;
     private String estado;
-    private Integer dniUser;
     private String horaInicio;
     private String horaFin;
 
     public EstacionamientoResponseDTO(Estacionamiento estacionamiento) {
         this.id = estacionamiento.getId();
         this.patente = estacionamiento.getPatente();
-        this.contraseña = estacionamiento.getContraseña();
         this.estado = estacionamiento.getEstado().toString();
-        this.dniUser = estacionamiento.getDniUser();
         this.horaInicio = estacionamiento.getHoraInicio() != null ? estacionamiento.getHoraInicio().toString() : null;
         this.horaFin = estacionamiento.getHoraFin() != null ? estacionamiento.getHoraFin().toString() : null;
     }
@@ -39,28 +35,12 @@ public class EstacionamientoResponseDTO {
         this.patente = patente;
     }
 
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
-
     public String getEstado() {
         return estado;
     }
 
     public void setEstado(String estado) {
         this.estado = estado;
-    }
-
-    public Integer getDniUser() {
-        return dniUser;
-    }
-
-    public void setDniUser(Integer dniUser) {
-        this.dniUser = dniUser;
     }
 
     public String getHoraInicio() {
@@ -85,7 +65,6 @@ public class EstacionamientoResponseDTO {
                 "id=" + id +
                 ", patente='" + patente + '\'' +
                 ", estado='" + estado + '\'' +
-                ", dniUser=" + dniUser +
                 ", horaInicio='" + horaInicio + '\'' +
                 ", horaFin='" + horaFin + '\'' +
                 '}';
