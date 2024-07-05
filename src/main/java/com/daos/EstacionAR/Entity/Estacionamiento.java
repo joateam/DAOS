ff8c13b5144fb10b84527fa5f84594e068a9031d
NaySauer <100716950+NaySauer@users.noreply.github.com>
2024-07-05 14:34:47 -0300

    id user

--- src/main/java/com/daos/EstacionAR/Entity/Estacionamiento.java
+++ src/main/java/com/daos/EstacionAR/Entity/Estacionamiento.java
@@ -25,6 +25,10 @@ public class Estacionamiento {
     private LocalDateTime horaInicio;
 
     private LocalDateTime horaFin;
+
+    // USER
+    private Long userId;
+
 //-------------------------------------------------------------------------
 
     public enum Estado {
@@ -75,6 +79,15 @@ public class Estacionamiento {
     public void setHoraFin(LocalDateTime horaFin) {
         this.horaFin = horaFin;
     }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
 
     @Override
     public String toString() {
