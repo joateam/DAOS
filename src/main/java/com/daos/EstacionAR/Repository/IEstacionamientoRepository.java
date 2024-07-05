f46843f0c6e9d30188db88df144918aab1fb5847
NaySauer <100716950+NaySauer@users.noreply.github.com>
2024-07-04 21:47:52 -0300

    repository querys

--- src/main/java/com/daos/EstacionAR/Repository/IEstacionamientoRepository.java
+++ src/main/java/com/daos/EstacionAR/Repository/IEstacionamientoRepository.java
@@ -2,9 +2,20 @@ package com.daos.EstacionAR.Repository;
 
 import com.daos.EstacionAR.Entity.Estacionamiento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface IEstacionamientoRepository extends JpaRepository<Estacionamiento, Long> {
-    Optional<Estacionamiento> findByPatente(String patente);
+    
+    @Query("SELECT e FROM Estacionamiento e WHERE e.vehiculo.patente = ?1 AND e.estado = 'ESTACIONADO'")
+    Optional<Estacionamiento> findEstacionadoByPatente(String patente);
+
+    Optional<Estacionamiento> findByVehiculoPatenteAndEstado(String patente, String estado);
+
+    Optional<Estacionamiento> findByVehiculoPatente(String patente);
+
 }
 
