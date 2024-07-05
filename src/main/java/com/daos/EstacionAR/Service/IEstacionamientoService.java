7e781644bbb647f98998e88fe1dde290a23c5dc0
NaySauer <100716950+NaySauer@users.noreply.github.com>
2024-07-04 22:24:13 -0300

    metodos

--- src/main/java/com/daos/EstacionAR/Service/IEstacionamientoService.java
+++ src/main/java/com/daos/EstacionAR/Service/IEstacionamientoService.java
@@ -1,19 +1,10 @@
 package com.daos.EstacionAR.Service;
 
-import java.util.Optional;
-
-import com.daos.EstacionAR.Controller.EstacionamientoForm;
 import com.daos.EstacionAR.Entity.Estacionamiento;
 
 public interface IEstacionamientoService {
 
-    void validarEstacionamiento(EstacionamientoForm estacionamientoForm) throws Exception;
-
-    Estacionamiento validarYCrearEstacionamiento(EstacionamientoForm estacionamientoForm) throws Exception;
-
-    Estacionamiento crearEstacionamiento(EstacionamientoForm estacionamientoForm);
-
-    Optional<Estacionamiento> actualizarEstacionamiento(String patente, EstacionamientoForm estacionamientoForm);
-
-    Optional<Estacionamiento> consultarEstacionamiento(String patente);
+    Estacionamiento insertarEstacionamiento(Estacionamiento estacionamiento);
+    Estacionamiento finalizarEstacionamiento(String patente, String password);
+    Estacionamiento consultarEstado(String patente);
 }
